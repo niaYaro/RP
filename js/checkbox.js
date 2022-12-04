772d3cf549c054b99eefb3c626abc02cb8610f4a
niaYaro <yarovyi@gmail.com>
2022-12-04 13:12:36 +0200

    remove commented code in checkbox.js

--- js/checkbox.js
+++ js/checkbox.js
@@ -24,23 +24,3 @@ function switchCheckbox(event) {
     taskContainer.innerHTML = '';
     renderTast(tasks)
 }
-// const tasksStatuses = document.querySelectorAll('.description');
-// function switchCheckbox() {
-//     for (let i = 0; i < tasksStatuses.length; i++) {
-//         console.log(tasksStatuses);
-//         if (tasksStatuses[i].innerHTML === 'Inactive') {
-//             tasksStatuses[i].innerHTML = 'Active'
-//             } else tasksStatuses[i].innerHTML = 'Inactive'
-//     }
-// }
-// (function() {
-
-//     const tasksStatuses = document.querySelector('.description');
-//     const toggle = document.querySelector('.status__checkbox-slider');
-//     toggle.addEventListener('click', switcher);
-//     function switcher() {
-//         if (tasksStatuses[i].innerHTML === 'Inactive') {
-//             tasksStatuses[i].innerHTML = 'Active'
-//             } else tasksStatuses[i].innerHTML = 'Inactive'
-//     }
-// })()
