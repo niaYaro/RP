c12b932b84ec0824ce83bcab69e1a108c60ea293
niaYaro <yarovyi@gmail.com>
2022-12-13 21:25:55 +0200

    Added functionality of getting data from local storage

--- js/save-data.js
+++ js/save-data.js
@@ -3,3 +3,12 @@ saveButton.addEventListener('click', saveDataInLocalStorage);
 function saveDataInLocalStorage () {
     localStorage.setItem("allTasks", JSON.stringify(tasks))
 }
+// const takeButton = document.querySelector('.storageback__wrapper-button');
+// takeButton.addEventListener('click', takeDataInLocalStorage);
+function takeDataInLocalStorage() {
+    const savedData = JSON.parse(window.localStorage.getItem('allTasks'));
+    tasks = savedData;
+    taskContainer.innerHTML = '';
+    renderTast(tasks);
+}
+takeDataInLocalStorage()
