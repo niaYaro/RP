1bbc4bac71d18a779aaecd3ffbe84df1051fa8a4
niaYaro <yarovyi@gmail.com>
2022-12-14 17:46:52 +0200

    Added functionality of filtering tasks by status

---
+++ js/sort-task-by-status.js
@@ -0,0 +1,34 @@
+const activeFilterBox = document.querySelector('.cycles__rigth-active');
+const inactiveFilterBox = document.querySelector('.cycles__rigth-inactive');
+activeFilterBox.addEventListener('click', filteredByActiveStatus)
+inactiveFilterBox.addEventListener('click', filteredByInactiveStatus)
+console.log(activeFilterBox.value);
+console.log(inactiveFilterBox.checked);
+function filteredByActiveStatus() {
+    const tasksWithActiveStatus = tasks.filter(function (task){
+        return task.status === 'Active';
+    })
+    if (activeFilterBox.checked === true){
+        inactiveFilterBox.disabled = true;
+        taskContainer.innerHTML = '';
+        renderTast(tasksWithActiveStatus);
+    } else {
+        inactiveFilterBox.disabled = false;
+        taskContainer.innerHTML = '';
+        renderTast(tasks);
+    }
+}
+function filteredByInactiveStatus() {
+    const tasksWithInactiveStatus = tasks.filter(function (task){
+        return task.status === 'Inactive';
+    })
+    if (inactiveFilterBox.checked === true){
+        activeFilterBox.disabled = true;
+        taskContainer.innerHTML = '';
+        renderTast(tasksWithInactiveStatus);
+    } else {
+        activeFilterBox.disabled = false;
+        taskContainer.innerHTML = '';
+        renderTast(tasks);
+    }
+}
